c9d53d06fdde9904ecf591743cbce3b594a442f9
Daniel <danonienko8@gmail.com>
2024-07-03 13:26:39 +0300

    Update: A lotta fixes and error handling improvements

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,5 +1,5 @@
-const { Events, EmbedBuilder, Colors} = require('discord.js');
-const {setTimeout: wait} = require("node:timers/promises");
+const { Events, EmbedBuilder, Colors } = require('discord.js');
+const { setTimeout: wait } = require("node:timers/promises");
 
 module.exports = {
     name: Events.InteractionCreate,
@@ -17,6 +17,25 @@ module.exports = {
             await command.execute(interaction);
         } catch (error) {
             console.log(error);
+
+            await interaction.channels.cache.get('1235938304990380113').send({
+                embeds: [
+                    new EmbedBuilder()
+                        .setTitle('Bot encountered an error!')
+                        .setDescription(`Someone ran a ${interaction.commandName} command and it errored!`)
+                        .setColor(Colors.Red)
+                        .setFields([
+                            { name: 'Error message', value: `\`\`\`js\n${error}\`\`\`` }
+                        ])
+                        .setFooter({
+                            text: `Chaos Forces Alliance`,
+                            iconURL: interaction.guild.iconURL()
+                        })
+                        .setTimestamp()
+                ],
+                allowedMentions: { parse: ["users"] },
+                content: '<@427832787605782549>'
+            })
 
             const replied = interaction.replied
             const replyData = {
