590972499ed53fb82ee9444daa6d30be686def09
Daniel <danonienko8@gmail.com>
2024-07-05 18:22:14 +0300

    And a little more...

--- commands/utility/pull.js
+++ commands/utility/pull.js
@@ -12,6 +12,8 @@ module.exports = {
         const allowedIDs = ["1226408360551645254", "427832787605782549"]
 
         if (interaction.member.roles.cache.hasAny(...allowedIDs) || allowedIDs.includes(interaction.member.id)) {
+            interaction.reply('`Pulling...`');
+            
             exec(`git pull https://${process.env.GIT_USERNAME}:${process.env.GIT_TOKEN}@gitlab.astrohweston.xyz/high-science/chaos-forces-alliance/cfa-manager master`, async (error, stdout, stderr) => {
                 if (error) {
                     console.log(`error: ${error}`);
