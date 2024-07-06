1b1185eaaec2ef7adb6bee229543efbf2d168d40
Daniel <danonienko8@gmail.com>
2024-07-06 14:22:47 +0300

    Update(deploy.js): This should hopefully fix the deploy command

--- commands/utility/deploy.js
+++ commands/utility/deploy.js
@@ -1,4 +1,4 @@
-const { exec } = require('child_process');
+const { spawn } = require('child_process');
 const { SlashCommandBuilder, EmbedBuilder, Colors } = require('discord.js');
 
 module.exports = {
@@ -10,53 +10,59 @@ module.exports = {
         if (interaction.member.roles.cache.hasAny(...allowedIDs) || allowedIDs.includes(interaction.member.id)) {
             interaction.reply('`Re-Deploying...`')
 
-            exec('node deploy-commands.js', (error, stdout, stderr) => {
-                if (error) {
-                    console.log(`error: ${error.message}`);
-                    return interaction.editReply({
-                        embeds: [
-                            new EmbedBuilder()
-                                .setColor(Colors.Red)
-                                .setTitle('Deploy Failed')
-                                .setDescription(`\`\`\`\n${error.message}\`\`\``)
-                                .setTimestamp()
-                                .setFooter({ text: interaction.guild.name })
-                        ]
-                    });
-                }
-                if (stderr) {
-                    console.log(`stderr: ${stderr}`);
-                    return interaction.followUp({
-                        embeds: [
-                            new EmbedBuilder()
-                                .setColor(Colors.Red)
-                                .setTitle('stderr')
-                                .setDescription('There are some warnings or information that console gave')
-                                .setFields([
-                                    { name: 'Output', value: `\`\`\`\n${stderr}\`\`\`` },
-                                ])
-                                .setTimestamp()
-                                .setFooter({ text: interaction.guild.name })
-                        ]
-                    });
-                }
-                console.log(`stdout: ${stdout}`);
-                return interaction.followUp({
+            const { stdout, stderr } = spawn('node deploy-commands', { shell: true })
+            // if (error) {
+            //     console.log(`error: ${error.message}`);
+            //     return interaction.editReply({
+            //         embeds: [
+            //             new EmbedBuilder()
+            //                 .setColor(Colors.Red)
+            //                 .setTitle('Deploy Failed')
+            //                 .setDescription(`\`\`\`\n${error.message}\`\`\``)
+            //                 .setTimestamp()
+            //                 .setFooter({ text: interaction.guild.name })
+            //         ]
+            //     });
+            // }
+
+            stderr.on('data', async (data) => {
+                console.log(`stderr: ${data}`);
+
+                await interaction.editReply({
+                    embeds: [
+                        new EmbedBuilder()
+                            .setColor(Colors.Red)
+                            .setTitle('stderr')
+                            .setDescription('There are some warnings or information that console gave')
+                            .setFields([
+                                { name: 'Output', value: `\`\`\`\n${data}\`\`\`` },
+                            ])
+                            .setTimestamp()
+                            .setFooter({ text: interaction.guild.name })
+                    ]
+                });
+
+            })
+
+            stdout.on('data', (data) => {
+                console.log(`stdout: ${data}`);
+
+                return interaction.editReply({
                     embeds: [
                         new EmbedBuilder()
                             .setColor(Colors.Green)
                             .setTitle('Deploy Successful')
                             .setDescription('Re-deployed commands successfully!')
                             .setFields([
-                                { name: 'Output', value: `\`\`\`\n${stdout}\`\`\`` },
+                                { name: 'Output', value: `\`\`\`\n${data}\`\`\`` },
                             ])
                             .setTimestamp()
                             .setFooter({ text: interaction.guild.name })
                     ]
                 });
-            });
+            })
         } else {
-            return interaction.reply({ content: 'You do not have permission to use this command!', ephemeral: true });
+            return interaction.reply({ content: 'You do not have permission to use this command!' });
         }
     }
 }
