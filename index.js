42a15830af98d3118d8929ab6bd667961479e098
Astro <maxsimilijano@gmail.com>
2024-11-18 09:37:30 +0100

    Revert "what kind of crack was daniel smoking"
    This reverts commit 58fc204782e85f60cafd459a4c8bbb6eea85557a.

--- index.js
+++ index.js
@@ -3,49 +3,68 @@ const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const knex = require("knex");
 require('dotenv').config();
+const { EmbedBuilder, Colors } = require('discord.js');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+try {
+    const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.commands = new Collection();
-const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
+    client.commands = new Collection();
+    const foldersPath = path.join(__dirname, 'commands');
+    const commandFolders = fs.readdirSync(foldersPath);
 
-for (const folder of commandFolders) {
-    const commandsPath = path.join(foldersPath, folder);
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-    for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-        const command = require(filePath);
-        if ('data' in command && 'execute' in command) {
-            client.commands.set(command.data.name, command);
-        } else {
-            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    for (const folder of commandFolders) {
+        const commandsPath = path.join(foldersPath, folder);
+        const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+        for (const file of commandFiles) {
+            const filePath = path.join(commandsPath, file);
+            const command = require(filePath);
+            if ('data' in command && 'execute' in command) {
+                client.commands.set(command.data.name, command);
+            } else {
+                console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+            }
         }
     }
-}
 
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+    const eventsPath = path.join(__dirname, 'events');
+    const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-for (const file of eventFiles) {
-    const filePath = path.join(eventsPath, file);
-    const event = require(filePath);
-    if (event.once) {
-        client.once(event.name, (...args) => event.execute(...args));
-    } else {
-        client.on(event.name, (...args) => event.execute(...args));
+    for (const file of eventFiles) {
+        const filePath = path.join(eventsPath, file);
+        const event = require(filePath);
+        if (event.once) {
+            client.once(event.name, (...args) => event.execute(...args));
+        } else {
+            client.on(event.name, (...args) => event.execute(...args));
+        }
     }
+
+    client.knex = knex({
+        client: process.env.DB_CLIENT,
+        connection: {
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME
+        }
+    });
+
+    client.login(process.env.DISCORD_TOKEN);
+} catch (error) {
+    console.log(error);
+
+    new Client().channels.fetch('1258036097422852248').then(channel => {
+        channel.send({
+            embeds:
+                [
+                    new EmbedBuilder()
+                        .setColor(Colors.Red)
+                        .setTitle('Error')
+                        .setDescription(`\`\`\`\n${error}\`\`\``)
+                        .setTimestamp()
+                        .setFooter({ text: channel.guild.name })
+                ]
+        })
+    })
 }
-
-client.knex = knex({
-    client: process.env.DB_CLIENT,
-    connection: {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME
-    }
-});
-
-client.login(process.env.DISCORD_TOKEN);
