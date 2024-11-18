c7d2203ee431afc8420d20565a2f4fb4f4a8e5bf
Astro <maxsimilijano@gmail.com>
2024-11-18 09:47:22 +0100

    i hate this codebase

--- commands/utility/dm.js
+++ commands/utility/dm.js
@@ -26,80 +26,84 @@ module.exports = {
             const allowedIDs = ["1239137720669044766", "1255634139730935860", "1071373709157351464"] //llasat one is astro
 
             if (interaction.member.roles.cache.hasAny(...allowedIDs) || allowedIDs.includes(interaction.member.id)) {
-                const user = interaction.options.getUser('member');
-                const message = interaction.options.getString('message');
-                const attachment = interaction.options.getAttachment('attachment');
+                const user = interaction.options.getUser('member', true);
+                const message = interaction.options.getString('message', false);
+                const attachment = interaction.options.getAttachment('attachment', false);
 
                 await interaction.guild.channels.cache.get(process.env.DM_LOG_CHANNEL_ID).send({
+                    embeds: [
+                        new EmbedBuilder()
+                            .setTitle(`New direct message!`)
+                            .setDescription(`<@${interaction.user.id}> has sent a new DM to <@${user.id}>!`)
+                            .setColor(Colors.Green)
+                            .setFields([
+                                { name: 'Text Content', value: message ? message : 'N/A' },
+                                { name: 'Attachment', value: attachment ? attachment.url : 'N/A' }
+                            ])
+                            .setFooter({
+                                text: interaction.guild.name,
+                                iconURL: interaction.guild.iconURL()
+                            })
+                            .setTimestamp()
+                    ]
+                });
+
+                if (user) {
+                    await user.send({
+                        content: message ? message : '** **',
+                        files: attachment ? [attachment] : []
+                    })
+                        .then(async () => {
+                            await interaction.editReply({
+                                embeds: [
+                                    new EmbedBuilder()
+                                        .setTitle('Message sent!')
+                                        .setDescription(`Message has been sent to <@${user.id}>!`)
+                                        .setColor(Colors.Green)
+                                        .setFooter({
+                                            text: interaction.guild.name,
+                                            iconURL: interaction.guild.iconURL()
+                                        })
+                                        .setTimestamp()
+                                ]
+                            });
+                        })
+                        .catch(async (_) => {
+                            console.log();
+
+                            return await interaction.editReply({
+                                embeds: [
+                                    new EmbedBuilder()
+                                        .setTitle('Error')
+                                        .setDescription(`An error was encountered while trying to send a message to <@${user.id}>!\nErrors: ${_}`)
+                                        .setColor(Colors.Red)
+                                        .setFooter({
+                                            text: interaction.guild.name,
+                                            iconURL: interaction.guild.iconURL()
+                                        })
+                                        .setTimestamp()
+                                ]
+                            });
+                        });
+                }
+            } else {
+                return interaction.editReply({
                     embeds:
                         [
                             new EmbedBuilder()
-                                .setTitle(`${interaction.user.tag} sent a DM to ${user.tag}`)
-                                .setColor(Colors.Green)
-                                .setFields([
-                                    { name: 'Text Content', value: message ? message : 'N/A' },
-                                    { name: 'Attachment', value: attachment ? attachment.url : 'N/A' }
-                                ])
+                                .setTitle('Permission Denied!')
+                                .setDescription('You do not have the required permissions to use this command!')
+                                .setColor(Colors.Red)
                                 .setFooter({
                                     text: interaction.guild.name,
                                     iconURL: interaction.guild.iconURL()
                                 })
                                 .setTimestamp()
                         ]
-                });
-
-                if (user) {
-                    user.send({
-                        content: message ? message : '** **',
-                        files: attachment ? [attachment] : []
-                    });
-
-                    await interaction.editReply({ content: `Sent DM to ${user.tag}!` })
-
-                if (userid) {
-                    const user = await interaction.client.users.fetch(userid);
-
-                    user.send({
-                        content: message ? message : '** **',
-                        files: attachment ? [attachment] : []
-
-                    return interaction.guild.channels.cache.get('1258036069572808725').send({
-                        embeds:
-                            [
-                                new EmbedBuilder()
-                                    .setTitle(`${interaction.user.tag} sent a DM to ${user.tag}`)
-                                    .setColor(Colors.Green)
-                                    .setFields([
-                                        { name: 'Text Content', value: message ? message : 'N/A' },
-                                        { name: 'Attachment', value: attachment ? attachment.url : 'N/A' }
-                                    ])
-                                    .setFooter({
-                                        text: interaction.guild.name,
-                                        iconURL: interaction.guild.iconURL()
-                                    })
-                                    .setTimestamp()
-                            ]
-                    });
-
-                } else {
-                    return interaction.editReply({
-                        embeds:
-                            [
-                                new EmbedBuilder()
-                                    .setTitle('Permission Denied!')
-                                    .setDescription('You do not have the required permissions to use this command!')
-                                    .setColor(Colors.Red)
-                                    .setFooter({
-                                        text: interaction.guild.name,
-                                        iconURL: interaction.guild.iconURL()
-                                    })
-                                    .setTimestamp()
-                            ]
-                    })
-                }
+                })
             }
         } catch (error) {
-            console.warn(error);
+            console.log(error);
 
             await interaction.guild.channels.cache.get(process.env.ERR_LOG_CHANNEL_ID).send({
                 embeds: [
@@ -116,8 +120,7 @@ module.exports = {
                         })
                         .setTimestamp()
                 ],
-                allowedMentions: { parse: ["users"] },
-                content: '<@427832787605782549>'
+                allowedMentions: { parse: ["users"] }
             })
 
             return interaction.editReply({ content: 'There was an error while executing this command! Contact Danonienko if error persists.', ephemeral: true });
